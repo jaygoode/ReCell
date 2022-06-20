687d105b5cfbe97f9c2f5ae8e63b0920ca2ad044
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-06-20 18:56:30 +0300

    added full project

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import Navbar from "./components/Navbar";
+import Modal from "./components/Modal";
+import CartContainer from "./components/CartContainer";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+
+function App() {
+  const dispatch = useDispatch();
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
